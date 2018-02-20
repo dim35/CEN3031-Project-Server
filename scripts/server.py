f61093e727c606a549ed5b8b3457f06091b9b24c
Steven Spinner <stevenmspin1003@gmail.com>
2018-02-20 17:34:06 +0000

    Add check if username is alredy in use

--- scripts/server.py
+++ scripts/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import sys
+import sqlite3
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -8,6 +9,18 @@ def createuser():
 #     print(request.form['username'], request.form['psw'], request.form['psw-repeat'], file=sys.stderr)
      if request.form['psw'] != request.form['psw-repeat']:
           return app.send_static_file('createacc_passwords_do_not_match.html')
+
+     conn = sqlite3.connect('/home/ubuntu/database.db')
+     c = conn.cursor()
+     t = (request.form['username'], )
+     c.execute("SELECT * from users WHERE usr=?", t)
+     out = c.fetchone()
+
+     if(out != None):
+         return app.send_static_file("createacc_username_in_use.html")
+
+     conn.close()
+
      return('Passwords match!')
 
 @app.route('/')
