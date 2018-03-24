7a5756dd5c8afb0443cf046c1a579edb28d84a1d
RustyBamboo <dvolya@gmail.com>
2018-03-24 13:27:46 -0400

    Small fixes

--- web/server.py
+++ web/server.py
@@ -3,25 +3,26 @@ from datetime import datetime
 import sys
 import sqlite3
 import hashlib
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/api/login', methods=['POST'])
 def login():
      username = request.form['username']
-     psw = rewquest.form['psw']
+     psw = request.form['psw']
 
      conn = sqlite3.connect('/home/ubuntu/database.db')
      c = conn.cursor()
 
-     out = c.execute("SELECT usr, psw FROM users WHERE usr=? AND hash=?", username, hashlib.sha512(psw.encode('utf-8')).hexdigest())
+     out = c.execute("SELECT usr, hash FROM users WHERE usr=? AND hash=?", [username, hashlib.sha512(psw.encode('utf-8')).hexdigest()])
      conn.close()
 
      if(out != None):
-          return -1
+         return json.dumps({"error":"not found"}), 666
      else:
           # Generate the session token
-          return hashlib.sha512(username + str(datetime.now()))
+          return json.dumps({"token": hashlib.sha512(username + str(datetime.now())).hexdigest()}), 200
 
 @app.route('/api/createuser', methods=['POST'])
 def createuser():
