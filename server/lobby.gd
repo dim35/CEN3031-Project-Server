b462540b896239face033161afc80c6e1be92cb9
RustyBamboo <dvolya@gmail.com>
2018-03-25 11:39:46 -0400

    Better output for server lobby

--- server/lobby.gd
+++ server/lobby.gd
@@ -13,10 +13,10 @@ func _ready():
 	get_tree().set_network_peer(peer)
 	get_tree().set_meta("network_peer", peer)
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
-	print('Test')
+	print('Started server...')
 	
 func _player_disconnected(id):
-	print(player_info[id]["username"] + " disconnected")
+	print(str(id) + "(" + player_info[id]["username"] + " " + player_info[id]["classtype"] + ") disconnected")
 	player_info.erase(id)
 	
 remote func register_player(id, info):
@@ -24,7 +24,7 @@ remote func register_player(id, info):
 	player_info[id] = info
 	# If I'm the server, let the new guy know about existing players
 	if get_tree().is_network_server():
-		print (info["username"] + " connected")
+		print (str(id) + "(" + info["username"] + " " + info["classtype"] + ") connected")
 		# Send the info of existing players
 		for peer_id in player_info:
 			rpc_id(id, "register_player", peer_id, player_info[peer_id])
