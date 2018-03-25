fa496920ffa484353ad78b733032d99b52cc867a
RustyBamboo <dvolya@gmail.com>
2018-03-25 09:30:01 -0400

    Test connection

--- server/Node2D.gd
+++ server/Node2D.gd
@@ -6,12 +6,23 @@ var MAX_PLAYERS = 5
 # var a = 2
 # var b = "textvar"
 
-var players = []
+var player_info = {}
 func _ready():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(SERVER_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(peer)
+	get_tree().set_meta("network_peer", peer)
 	print('Test')
+	
+remote func register_player(id, info):
+	# Store the info
+	player_info[id] = info
+	# If I'm the server, let the new guy know about existing players
+	if get_tree().is_network_server():
+		print ('New player')
+		# Send the info of existing players
+		for peer_id in player_info:
+			rpc_id(id, "register_player", peer_id, player_info[peer_id])
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
