db60191f4dd24caba5d7dcb36a6f0c53255cf00a
RustyBamboo <dvolya@gmail.com>
2018-03-25 08:58:24 -0400

    from ready to init

--- server/Node.gd
+++ server/Node.gd
@@ -1,23 +1,23 @@
 extends Node
 
+var SERVER_PORT = 5555
+var MAX_PLAYERS = 5
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
+var players = []
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(SERVER_PORT, MAX_PLAYERS)
+	get_tree().set_network_peer(peer)
+	print('Test')
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
-var players = []
-
 func _init():
-    var host = NetworkedMultiplayerENet.new()
-    host.create_server(9000, 64)
-    get_tree().set_network_peer(host)
-    print('Test')
+	pass
+
