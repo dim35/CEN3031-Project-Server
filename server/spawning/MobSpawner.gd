3def3ff2c51f9e479b012b081f5922c4d7cab3f6
RustyBamboo <dvolya@gmail.com>
2018-04-19 09:37:20 -0400

    Add way to limit number of total spawned from a spawner for a level

--- server/spawning/MobSpawner.gd
+++ server/spawning/MobSpawner.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# maximum number of mobs that can spawn from a point during the level
+const MAX_SPAWNED_FROM_POINT = 20
+
 onready var mob = load("res://server/entity/Mob.gd")
 var mobs = null
 var players = null
@@ -10,7 +13,9 @@ func _ready():
 func _process(delta):
 	for p in players.get_children():
 		for spawn in p.cloest_mob_spawnpoints:
-			if (randi()%1000 + 1 == 5):
+			if spawn.total_spawned >= MAX_SPAWNED_FROM_POINT:
+				continue
+			if (randi()%500 + 1 == 5):
 				var id = randi()%1000000000 + 1
 				if id in mobs.get_children():
 					return
@@ -18,5 +23,6 @@ func _process(delta):
 				var m = mob.new()
 				m.set_name(str(id))
 				m.position = spawn.position
+				spawn.total_spawned+=1
 				mobs.add_child(m)
 				get_tree().get_root().get_node("World").spawn_mob(who, id)
